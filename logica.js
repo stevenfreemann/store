c43aa66696ff9293f16534a21ea3554a324178e1
Steven Rodriguez <ekoomediadev@gmail.com>
2025-03-07 14:53:13 -0500

    +

--- logica.js
+++ logica.js
@@ -1,40 +1,35 @@
 const data = [
-    { id: 1, name: "Producto 1", price: 60 },
-    { id: 2, name: "Producto 2", price: 100 },
-    { id: 3, name: "Producto 3", price: 120 },
-    { id: 4, name: "Producto 4", price: 70 },
-    { id: 4, name: "Producto 5", price: 30 },
-    { id: 4, name: "Producto 6", price: 10 },
+    { id: 1, name: "Producto 1", price: 6 },
+    { id: 2, name: "Producto 2", price: 6 },
+    { id: 3, name: "Producto 3", price: 3 },
+    { id: 4, name: "Producto 4", price: 3 },
 ]
 
 function findCombination(data, limit) {
     let bestCombination = { total: 0, products: [] }
 
-    function sumRecursive(startIndex, currentTotal, currentProducts) {
-        if (currentTotal > bestCombination.total && currentTotal <= limit) {
-            bestCombination = {
-                total: currentTotal,
-                products: [...currentProducts],
-            }
-        }
-
+    function sum(startIndex, currentTotal, currentProducts) {
         for (let i = startIndex; i < data.length; i++) {
             const product = data[i]
-            const newTotal = currentTotal + product.price
-
+            let newTotal = currentTotal + product.price
             if (newTotal <= limit) {
                 currentProducts.push(product)
-                sumRecursive(i + 1, newTotal, currentProducts)
-                return
+                currentTotal = newTotal
+            }
+            if (currentTotal > bestCombination.total && currentTotal <= limit) {
+                bestCombination = {
+                    total: currentTotal,
+                    products: [...currentProducts],
+                }
             }
         }
     }
 
     for (let start = 0; start < data.length; start++) {
-        sumRecursive(start, 0, [])
+        sum(start, 0, [])
     }
 
     return bestCombination.products
 }
 
-console.log(findCombination(data, 100))
+console.log(findCombination(data, 9))
